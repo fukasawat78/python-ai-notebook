484c02d0306a5198a05812ddc995c9a1d019557e
fukasawat <55072916+fukasawat78@users.noreply.github.com>
2022-02-02 03:39:05 +0900

    Create globals.py

---
+++ config/globals.py
@@ -0,0 +1,10 @@
+import os
+
+# Path settings
+HOME = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+CONF_DIR = os.path.join(HOME, "config")
+DATA_DIR = os.path.join(HOME, "datasets")
+
+# seed
+SEED = 1234
