10e5cbe0b1aa7d6ac8765844ebbfc1780272e665
fukasawat78 <fukasawat78@bbwcfs.local127.0.0.1>
2022-08-12 17:34:04 +0900

    modified

--- config/globals.py
+++ config/globals.py
@@ -4,7 +4,7 @@ import os
 HOME = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 CONF_DIR = os.path.join(HOME, "config")
-DATA_DIR = os.path.join(HOME, "datasets")
+DATA_DIR = os.path.join(HOME, "data")
 
 # seed
 SEED = 1234
